2813b10749122e3cec13865150bbdc945a289699
Giorgi Ubiria <ubiriagiorgi8@gmail.com>
2023-07-31 23:59:38 +0400

    Added type howler to the sound attribute on SoundInstance interface.

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -1,24 +1,28 @@
-'use client';
+"use client";
 
-import { BsPauseFill, BsPlayFill } from 'react-icons/bs';
-
-import MediaItem from "./MediaItem";
-import LikeButton from "./LikeButton";
+import useSound from "use-sound";
+import { useEffect, useState } from "react";
+import { BsPauseFill, BsPlayFill } from "react-icons/bs";
+import { HiSpeakerWave, HiSpeakerXMark } from "react-icons/hi2";
+import { AiFillStepBackward, AiFillStepForward } from "react-icons/ai";
 
 import { Song } from "@/types";
-import { AiFillStepBackward, AiFillStepForward } from 'react-icons/ai';
-import { HiSpeakerXMark, HiSpeakerWave } from 'react-icons/hi2';
-import Slider from './Slider';
-import usePlayer from '@/hooks/usePlayer';
-import { useEffect, useState } from 'react';
-import useSound from 'use-sound';
+import usePlayer from "@/hooks/usePlayer";
+
+import LikeButton from "./LikeButton";
+import MediaItem from "./MediaItem";
+import Slider from "./Slider";
+
 
 interface PlayerContentProps {
   song: Song;
   songUrl: string;
 }
 
-const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
+const PlayerContent: React.FC<PlayerContentProps> = ({
+  song,
+  songUrl
+}) => {
   const player = usePlayer();
   const [volume, setVolume] = useState(1);
   const [isPlaying, setIsPlaying] = useState(false);
@@ -28,11 +32,10 @@ const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
 
   const onPlayNext = () => {
     if (player.ids.length === 0) {
-      return 0;
+      return;
     }
 
     const currentIndex = player.ids.findIndex((id) => id === player.activeId);
-
     const nextSong = player.ids[currentIndex + 1];
 
     if (!nextSong) {
@@ -44,11 +47,10 @@ const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
 
   const onPlayPrevious = () => {
     if (player.ids.length === 0) {
-      return 0;
+      return;
     }
 
     const currentIndex = player.ids.findIndex((id) => id === player.activeId);
-
     const previousSong = player.ids[currentIndex - 1];
 
     if (!previousSong) {
@@ -58,15 +60,19 @@ const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
     player.setId(previousSong);
   }
 
-  const [play, { stop, sound }] = useSound(songUrl, {
-    volume: volume,
-    onplay: () => setIsPlaying(true),
-    onend: () => { setIsPlaying(false); onPlayNext(); },
-    onpause: () => setIsPlaying(false),
-    onstop: () => setIsPlaying(false),
-    format: ['mp3'],
-  });
-
+  const [play, { pause, sound }] = useSound(
+    songUrl,
+    {
+      volume: volume,
+      onplay: () => setIsPlaying(true),
+      onend: () => {
+        setIsPlaying(false);
+        onPlayNext();
+      },
+      onpause: () => setIsPlaying(false),
+      format: ['mp3']
+    }
+  );
 
   useEffect(() => {
     sound?.play();
@@ -74,15 +80,13 @@ const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
     return () => {
       sound?.unload();
     }
-  }, [sound])
+  }, [sound]);
 
   const handlePlay = () => {
     if (!isPlaying) {
       play();
-      console.log('is playing')
     } else {
-      stop();
-      console.log('is not playing')
+      pause();
     }
   }
 
@@ -103,24 +107,86 @@ const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
         </div>
       </div>
 
-      <div className="flex md:hidden col-auto w-full justify-end items-center">
-        <div onClick={handlePlay} className="h-10 w-10 flex items-center justify-center rounded-full bg-white p-1 cursor-pointer">
+      <div
+        className="
+            flex 
+            md:hidden 
+            col-auto 
+            w-full 
+            justify-end 
+            items-center
+          "
+      >
+        <div
+          onClick={handlePlay}
+          className="
+              h-10
+              w-10
+              flex 
+              items-center 
+              justify-center 
+              rounded-full 
+              bg-white 
+              p-1 
+              cursor-pointer
+            "
+        >
           <Icon size={30} className="text-black" />
         </div>
       </div>
 
-      <div className='hidden h-full md:flex justify-center items-center w-full max-w-[720px] gap-x-6'>
-        <AiFillStepBackward size={30} className='text-neutral-400 hover:text-white transition cursor-pointer'
-          onClick={onPlayPrevious} />
-        <div onClick={handlePlay} className='flex items-center justify-center h-10 w-10 rounded-full bg-white cursor-pointer p-1'>
-          <Icon size={30} className='text-black' />
+      <div
+        className="
+            hidden
+            h-full
+            md:flex 
+            justify-center 
+            items-center 
+            w-full 
+            max-w-[722px] 
+            gap-x-6
+          "
+      >
+        <AiFillStepBackward
+          onClick={onPlayPrevious}
+          size={30}
+          className="
+              text-neutral-400 
+              cursor-pointer 
+              hover:text-white 
+              transition
+            "
+        />
+        <div
+          onClick={handlePlay}
+          className="
+              flex 
+              items-center 
+              justify-center
+              h-10
+              w-10 
+              rounded-full 
+              bg-white 
+              p-1 
+              cursor-pointer
+            "
+        >
+          <Icon size={30} className="text-black" />
         </div>
-        <AiFillStepForward size={30} className='text-neutral-400 hover:text-white transition cursor-pointer'
-          onClick={onPlayNext} />
+        <AiFillStepForward
+          onClick={onPlayNext}
+          size={30}
+          className="
+              text-neutral-400 
+              cursor-pointer 
+              hover:text-white 
+              transition
+            "
+        />
       </div>
 
-      <div className='hidden md:flex w-full justify-end pr-2'>
-        <div className='flex items-center gap-x-2 w-[120px]'>
+      <div className="hidden md:flex w-full justify-end pr-2">
+        <div className="flex items-center gap-x-2 w-[120px]">
           <VolumeIcon
             onClick={toggleMute}
             className="cursor-pointer"
@@ -132,8 +198,9 @@ const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
           />
         </div>
       </div>
+
     </div>
-  )
+  );
 }
 
-export default PlayerContent
+export default PlayerContent;
