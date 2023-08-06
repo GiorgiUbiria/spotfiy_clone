411799cb0ee65cf2c49ef23e0980adc0a5441fc5
Giorgi Ubiria <ubiriagiorgi8@gmail.com>
2023-08-06 18:24:03 +0400

    Fully removed the stripe integration logic.

--- app/account/components/AccountContent.tsx
+++ app/account/components/AccountContent.tsx
@@ -4,14 +4,11 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
 import { useUser } from "@/hooks/useUser";
-import Button from "@/components/Button";
-import useSubscribeModal from "@/hooks/useSubscribeModal";
 import { postData } from "@/libs/helpers";
 
 const AccountContent = () => {
   const router = useRouter();
-  const subscribeModal = useSubscribeModal();
-  const { isLoading, subscription, user } = useUser();
+  const { isLoading, user } = useUser();
 
   const [loading, setLoading] = useState(false);
 
@@ -36,32 +33,7 @@ const AccountContent = () => {
 
   return (
     <div className="mb-7 px-6">
-      {!subscription && (
-        <div className="flex flex-col gap-y-4">
-          <p>No active plan.</p>
-          <Button
-            onClick={subscribeModal.onOpen}
-            className="w-[300px]"
-          >
-            Subscribe
-          </Button>
-        </div>
-      )}
-      {subscription && (
-        <div className="flex flex-col gap-y-4">
-          <p>You are currently on the
-            <b> {subscription?.prices?.products?.name} </b>
-            plan.
-          </p>
-          <Button
-            disabled={loading || isLoading}
-            onClick={redirectToCustomerPortal}
-            className="w-[300px]"
-          >
-            Open customer portal
-          </Button>
-        </div>
-      )}
+
     </div>
   );
 }
